8d84f2901ee6048bccddea4eb96daa519c299c07
Jeremiah Lantzer <jeremiahlantzer25@gmail.com>
2016-09-24 16:05:11 -0400

    Removed second drone script. Added video test code

--- python/drone1.py
+++ python/drone1.py
@@ -11,76 +11,71 @@ import time
 import ps_drone
 
 # /*/ Clean Start up Sequence /*/ #
-drone1 = ps_drone.Drone()               # initializes the drone
-drone1.startup()                        # connects the script to the drone
-drone1.reset()                          # reset the lights on the drone
+drone = ps_drone.Drone()                   # initializes the drone
+drone.startup()                            # connects the script to the drone
+drone.reset()                              # reset the lights on the drone
 
-while drone1.getBattery()[0] == -1:     # reset complete check
+while drone.getBattery()[0] == -1:         # reset complete check
     time.sleep(0.1)
 
-print "Battery: "+str(drone1.getBattery()[0])+"%  "+str(drone1.getBattery()[1])     # outputs current battery level
-drone1.useDemoMode(True)                                                            # 15 data sets per second
-drone1.getNDpackage(["demo", "vision detect"])                                      # packets to decode
-time.sleep(0.5)                                                                     # extra time to complete setup
+print "Battery: " + str(drone.getBattery()[0]) + "%  " + str(drone.getBattery()[1])   # outputs current battery level
+drone.useDemoMode(True)                                                                # 15 data sets per second
+drone.getNDpackage(["demo", "vision detect"])                                          # packets to decode
+time.sleep(0.5)                                                                         # extra time to complete setup
 # /*/ Clean Start up Sequence /*/ #
+
 
 app = Flask(__name__)
 
 
-@app.route('/<drone_ID>/right')
-def droneright(drone_ID):
-    if drone_ID == 1:
-        drone1.moveRight(0.5)
-        time.sleep(0.1)
+@app.route('/drone/right')
+def droneright():
+    drone.moveRight(0.5)
+    time.sleep(0.1)
 
 
-@app.route('/<drone_ID>/left')
-def droneleft(drone_ID):
-    if drone_ID == 1:
-        drone1.moveLeft(0.5)
-        time.sleep(0.1)
+@app.route('/drone/left')
+def droneleft():
+    drone.moveLeft(0.5)
+    time.sleep(0.1)
 
 
-@app.route('/<drone_ID>/forward')
-def droneforward(drone_ID):
-    if drone_ID == 1:
-        drone1.moveForward(0.5)
-        time.sleep(0.1)
+@app.route('/drone/forward')
+def droneforward():
+    drone.moveForward(0.5)
+    time.sleep(0.1)
 
 
-@app.route('/<drone_ID>/backward')
-def dronebackward(drone_ID):
-    if drone_ID == 1:
-        drone1.moveBackward(0.5)
-        time.sleep(0.1)
+@app.route('/drone/backward')
+def dronebackward():
+    drone.moveBackward(0.5)
+    time.sleep(0.1)
 
 
-@app.route('/<drone_ID>/up')
-def droneup(drone_ID):
-    if drone_ID == 1:
-        drone1.moveUp(0.5)
-        time.sleep(0.1)
+@app.route('/drone/up')
+def droneup():
+    drone.moveUp(0.5)
+    time.sleep(0.1)
 
 
-@app.route('/<drone_ID>/down')
-def dronedown(drone_ID):
-    if drone_ID == 1:
-        drone1.moveDown(0.5)
-        time.sleep(0.1)
+@app.route('/drone/down')
+def dronedown():
+    drone.moveDown(0.5)
+    time.sleep(0.1)
 
 
-@app.route('/<drone_ID>/turnright')
-def robotright(drone_ID):
-    if drone_ID == 1:
-        drone1.turnRight(0.5)
-        time.sleep(0.1)
+@app.route('/drone/turnright')
+def robotright():
+    drone.turnRight(0.5)
+    time.sleep(0.1)
 
 
-@app.route('/<drone_ID>/turnleft')
-def robotleft(drone_ID):
-    if drone_ID == 1:
-        drone1.turnLeft(0.5)
-        time.sleep(0.1)
+@app.route('/drone/turnleft')
+def robotleft():
+    drone.turnLeft(0.5)
+    time.sleep(0.1)
+
 
 if __name__ == "__main__":
+    
     app.run()
